6bfbe153b32c0d0569e1c15abd0d88de3d04c1e2
Raghu <raghu15sep99@gmail.com>
2019-11-03 10:30:21 +0530

    Converted tabs to spaces

--- nn.py
+++ nn.py
@@ -3,194 +3,194 @@ import math
 
 class Brain:
 
-	def __init__(self, nn_config, activation='sigmoid', hidden_layers=None):
-		'''
-		Initializes the brain of the snake
-		4 output nodes: left, right, down, up
-		Attributes:
-			nn_config: describes the neural network configuration of hidden nodes
-				example: nn_config = [10, 3, 4] will create 2 hidden layers with 3 and 4 hidden nodes respectively and input layer has 10 input nodes.
-			hidden_layers: optional. used to specify determined params instead of random initializations
-			activation: Activation function used
-				available: 'sigmoid', 'relu'
-		'''
-		self.nn_config = nn_config
-		if hidden_layers:
-			self.hidden_layers = hidden_layers
-			return
-		hidden_layers = []
-		l = len(nn_config)
-		for i in range(1, l):
-			layer = Layer(nn_config[i-1], nn_config[i], activation=activation)
-			hidden_layers.append(layer)
-		# 4 output nodes
-		layer = Layer(nn_config[l-1], 4, True)
-		hidden_layers.append(layer)
-		self.hidden_layers = hidden_layers
-	
-	def move(self, percept):
-		'''
-		Function to provide output move given the input percept of the snake using feed-forward on the neural net.
-		Attributes:
-			percept: row vector of percept. dim: 1 * nx
-		'''
-		percept = np.array(percept).reshape(-1)
-		l = percept.shape[0]
-		if l != self.nn_config[0]:
-			raise ValueError('Invalid percept length')
-		# 1 for bias term
-		percept = np.append(percept, 1)
-		percept = percept.reshape(1, -1)
-		feed = percept
-		# Feed Forward
-		for layer in self.hidden_layers:
-			feed = layer.product(feed)
-		feed = feed.reshape(-1)
-		mx = np.max(feed)
-		# Return which move to take relative to the snake
-		moves = np.zeros(4)
-		for i, f in enumerate(feed):
-			if f == mx:
-				moves[i] = 1
-				break
-		return moves
-	
-	def clone(self):
-		'''
-		Returns a clone of the brain.
-		'''
-		new_layers = []
-		for layer in self.hidden_layers:
-			new_layer = layer.clone()
-			new_layers.append(new_layer)
-		b = Brain(self.nn_config, hidden_layers=new_layers)
-		return b
+    def __init__(self, nn_config, activation='sigmoid', hidden_layers=None):
+        '''
+        Initializes the brain of the snake
+        4 output nodes: left, right, down, up
+        Attributes:
+            nn_config: describes the neural network configuration of hidden nodes
+                example: nn_config = [10, 3, 4] will create 2 hidden layers with 3 and 4 hidden nodes respectively and input layer has 10 input nodes.
+            hidden_layers: optional. used to specify determined params instead of random initializations
+            activation: Activation function used
+                available: 'sigmoid', 'relu'
+        '''
+        self.nn_config = nn_config
+        if hidden_layers:
+            self.hidden_layers = hidden_layers
+            return
+        hidden_layers = []
+        l = len(nn_config)
+        for i in range(1, l):
+            layer = Layer(nn_config[i-1], nn_config[i], activation=activation)
+            hidden_layers.append(layer)
+        # 4 output nodes
+        layer = Layer(nn_config[l-1], 4, True)
+        hidden_layers.append(layer)
+        self.hidden_layers = hidden_layers
+    
+    def move(self, percept):
+        '''
+        Function to provide output move given the input percept of the snake using feed-forward on the neural net.
+        Attributes:
+            percept: row vector of percept. dim: 1 * nx
+        '''
+        percept = np.array(percept).reshape(-1)
+        l = percept.shape[0]
+        if l != self.nn_config[0]:
+            raise ValueError('Invalid percept length')
+        # 1 for bias term
+        percept = np.append(percept, 1)
+        percept = percept.reshape(1, -1)
+        feed = percept
+        # Feed Forward
+        for layer in self.hidden_layers:
+            feed = layer.product(feed)
+        feed = feed.reshape(-1)
+        mx = np.max(feed)
+        # Return which move to take relative to the snake
+        moves = np.zeros(4)
+        for i, f in enumerate(feed):
+            if f == mx:
+                moves[i] = 1
+                break
+        return moves
+    
+    def clone(self):
+        '''
+        Returns a clone of the brain.
+        '''
+        new_layers = []
+        for layer in self.hidden_layers:
+            new_layer = layer.clone()
+            new_layers.append(new_layer)
+        b = Brain(self.nn_config, hidden_layers=new_layers)
+        return b
 
-	def mutate(self, prob):
-		'''
-		Perfrom mutation on all layers
-		Attributes:
-			prob: mutation rate
-		'''
-		for layer in self.hidden_layers:
-			layer.mutate(prob)
+    def mutate(self, prob):
+        '''
+        Perfrom mutation on all layers
+        Attributes:
+            prob: mutation rate
+        '''
+        for layer in self.hidden_layers:
+            layer.mutate(prob)
 
-	def cross_over(self, partner):
-		'''
-		Performs genetic crossover operation on this chromosome with partner chromosome.
-		Attributes:
-			partner: Brain of the partner snake
-		Note: nn_config of partner must be same as 'this' brain
-		'''
-		if self.nn_config != partner.nn_config:
-			raise ValueError('Brains incompatible for crossover. nn_config must be same')
-		
-		new_hidden_layers = []
-		for i, layer in enumerate(self.hidden_layers):
-			new_layer = layer.cross_over(partner.hidden_layers[i])
-			new_hidden_layers.append(new_layer)
-		# Child brain after crossover
-		b = Brain(self.nn_config, hidden_layers=new_hidden_layers)
-		return b
+    def cross_over(self, partner):
+        '''
+        Performs genetic crossover operation on this chromosome with partner chromosome.
+        Attributes:
+            partner: Brain of the partner snake
+        Note: nn_config of partner must be same as 'this' brain
+        '''
+        if self.nn_config != partner.nn_config:
+            raise ValueError('Brains incompatible for crossover. nn_config must be same')
+        
+        new_hidden_layers = []
+        for i, layer in enumerate(self.hidden_layers):
+            new_layer = layer.cross_over(partner.hidden_layers[i])
+            new_hidden_layers.append(new_layer)
+        # Child brain after crossover
+        b = Brain(self.nn_config, hidden_layers=new_hidden_layers)
+        return b
 
 class Layer:
 
-	def __init__(self, nx, ny, is_output=False, activation='sigmoid'):
-		'''
-		Initializes the matrix for the layer
-		Attributes:
-			nx: number of rows
-			ny: number of columns
-			is_output: determines output layer or not
-		'''
-		# plus 1 for bias
-		nx += 1
-		self.nx = nx
-		self.ny = ny
-		# Random values between [-1, 1]
-		mat = np.random.random(size=(nx, ny)) * 2 - 1
-		self.mat = mat
-		self.is_output = is_output
-		self.activation = self.sigmoid
-		if activation == 'relu':
-			self.activation = self.relu
+    def __init__(self, nx, ny, is_output=False, activation='sigmoid'):
+        '''
+        Initializes the matrix for the layer
+        Attributes:
+            nx: number of rows
+            ny: number of columns
+            is_output: determines output layer or not
+        '''
+        # plus 1 for bias
+        nx += 1
+        self.nx = nx
+        self.ny = ny
+        # Random values between [-1, 1]
+        mat = np.random.random(size=(nx, ny)) * 2 - 1
+        self.mat = mat
+        self.is_output = is_output
+        self.activation = self.sigmoid
+        if activation == 'relu':
+            self.activation = self.relu
 
-	def product(self, vec):
-		'''
-		Matrix multiplication of input vector and current layer matrix
-		Attributes:
-			vec: input vector
-		'''
-		vec = vec.reshape(1, -1)
-		output = np.dot(vec, self.mat)
-		if self.is_output:
-			output = self.softmax(output)
-		else:
-			output = self.activation(output)
-			output = output.reshape(-1)
-			# 1 for bias term
-			output = np.append(output, 1)
-		return output
+    def product(self, vec):
+        '''
+        Matrix multiplication of input vector and current layer matrix
+        Attributes:
+            vec: input vector
+        '''
+        vec = vec.reshape(1, -1)
+        output = np.dot(vec, self.mat)
+        if self.is_output:
+            output = self.softmax(output)
+        else:
+            output = self.activation(output)
+            output = output.reshape(-1)
+            # 1 for bias term
+            output = np.append(output, 1)
+        return output
 
-	def clone(self):
-		'''
-		Returns a clone of the current layer
-		'''
-		layer = Layer(self.nx-1, self.ny, self.is_output, self.activation)
-		for i in range(self.nx):
-			for j in range(self.ny):
-				layer.mat[i][j] = self.mat[i][j]
-		return layer
+    def clone(self):
+        '''
+        Returns a clone of the current layer
+        '''
+        layer = Layer(self.nx-1, self.ny, self.is_output, self.activation)
+        for i in range(self.nx):
+            for j in range(self.ny):
+                layer.mat[i][j] = self.mat[i][j]
+        return layer
 
-	def mutate(self, prob):
-		'''
-		Perform mutation on weight matrix
-		'''
-		for i in range(self.nx):
-			for j in range(self.ny):
-				chance = float(np.random.random(1))
-				if chance <= prob:
-					change = float(np.random.random(1) * 2 - 1)
-					self.mat[i][j] += change
-				# Bounding weights in [-1, 1]
-				self.mat[i][j] = min(1, self.mat[i][j])
-				self.mat[i][j] = max(-1, self.mat[i][j])
+    def mutate(self, prob):
+        '''
+        Perform mutation on weight matrix
+        '''
+        for i in range(self.nx):
+            for j in range(self.ny):
+                chance = float(np.random.random(1))
+                if chance <= prob:
+                    change = float(np.random.random(1) * 2 - 1)
+                    self.mat[i][j] += change
+                # Bounding weights in [-1, 1]
+                self.mat[i][j] = min(1, self.mat[i][j])
+                self.mat[i][j] = max(-1, self.mat[i][j])
 
-	def cross_over(self, layer):
-		'''
-		Performs crossover on the 'this' layer with layer
-		Attributes:
-			layer: partner layer
-		'''
-		new_layer = self.clone()
-		x = np.random.randint(0, self.nx)
-		y = np.random.randint(0, self.ny)
-		for i in range(self.nx):
-			for j in range(self.ny):
-				if i * self.ny + j > x * self.ny + y:
-					new_layer.mat[i][j] = layer.mat[i][j]
-		return new_layer
+    def cross_over(self, layer):
+        '''
+        Performs crossover on the 'this' layer with layer
+        Attributes:
+            layer: partner layer
+        '''
+        new_layer = self.clone()
+        x = np.random.randint(0, self.nx)
+        y = np.random.randint(0, self.ny)
+        for i in range(self.nx):
+            for j in range(self.ny):
+                if i * self.ny + j > x * self.ny + y:
+                    new_layer.mat[i][j] = layer.mat[i][j]
+        return new_layer
 
-	def sigmoid(self, x):
-		'''
-		Perform sigmoid activation
-		'''
-		return 1 / (1 + np.exp(-x))
+    def sigmoid(self, x):
+        '''
+        Perform sigmoid activation
+        '''
+        return 1 / (1 + np.exp(-x))
 
-	def softmax(self, x):
-		'''
-		Compute softmax
-		'''
-		e_x = np.exp(x - np.max(x))
-		return e_x / np.sum(e_x)
-	
-	def relu(self, x):
-		'''
-		Perfom relu activation
-		'''
-		return np.maximum(x, 0, x)	
+    def softmax(self, x):
+        '''
+        Compute softmax
+        '''
+        e_x = np.exp(x - np.max(x))
+        return e_x / np.sum(e_x)
+    
+    def relu(self, x):
+        '''
+        Perfom relu activation
+        '''
+        return np.maximum(x, 0, x)	
 
 if __name__ == "__main__":
-	b = Brain([2, 4])
-	b.mutate(0.2)
-	print(b.move([1, 1]))
+    b = Brain([2, 4])
+    b.mutate(0.2)
+    print(b.move([1, 1]))
--- population.py
+++ population.py
@@ -10,171 +10,173 @@ nn_config = [8, 16]
 mutation_rate = 0.01
 
 class Population:
-	
-	def __init__(self, population_size):
-		'''
-		Creates a population of snakes
-		Attributes:
-			population_size: Size of the population
-		'''
-		self.population_size = population_size
-		self.snakes = []
-		self.globalBest = None
-		# Generate a random population
-		for _ in range(population_size):
-			self.snakes.append(Snake())
-	
-	def natural_selection(self):
-		'''
-		Peforms one iteration of natural selection to produce stable new generation.
-		Uses Roulette Wheel selection and random elitism
-		'''
-		fitness = []
-		# Total length of all snakes (for calculating average)
-		totlen = 0
-		for i, snake in enumerate(self.snakes):
-			snake.train()
-			fitness.append((snake.score, i))
-			totlen += snake.len
-		fitness.sort(reverse=True)
-		best = self.snakes[fitness[0][1]].clone()
-		self.best = best
-		# Store the global best of all times
-		if self.globalBest == None:
-			self.globalBest = best
-		if self.globalBest.score < best.score:
-			self.globalBest = best
-		print('Current best score: ', best.score)
-		print('Best length: ', best.len)
-		print('Average length: ', totlen / self.population_size)
-		new_population = [self.globalBest.clone()]
-		# Select best 'rand' individuals and keep them for next generation (random elitism)
-		rand = np.random.randint(1, self.population_size)
-		for i in range(1, rand):
-			new_population.append(self.snakes[fitness[i][1]])
-		# Shuffling for randomness
-		shuffle(fitness)
-		# Generate new snakes by crossover and mutation
-		for i in range(rand, self.population_size):
-			parent1 = self.select_snake(fitness)
-			parent2 = self.select_snake(fitness)
-			child = parent1.cross_over(parent2)
-			child.mutate(mutation_rate)
-			new_population.append(child)
-		self.snakes = new_population
+    
+    def __init__(self, population_size):
+        '''
+        Creates a population of snakes
+        Attributes:
+            population_size: Size of the population
+        '''
+        self.population_size = population_size
+        self.snakes = []
+        self.globalBest = None
+        # Generate a random population
+        for _ in range(population_size):
+            self.snakes.append(Snake())
+    
+    def natural_selection(self):
+        '''
+        Peforms one iteration of natural selection to produce stable new generation.
+        Uses Roulette Wheel selection and random elitism
+        '''
+        fitness = []
+        # Total length of all snakes (for calculating average)
+        totlen = 0
+        for i, snake in enumerate(self.snakes):
+            snake.train()
+            fitness.append((snake.score, i))
+            totlen += snake.len
+        fitness.sort(reverse=True)
+        best = self.snakes[fitness[0][1]].clone()
+        self.best = best
+        # Store the global best of all times
+        if self.globalBest == None:
+            self.globalBest = best
+        if self.globalBest.score < best.score:
+            self.globalBest = best
+        print('Current best score: ', best.score)
+        print('Best length: ', best.len)
+        print('Average length: ', totlen / self.population_size)
+        new_population = [self.globalBest.clone()]
+        # Select best 'rand' individuals and keep them for next generation (random elitism)
+        rand = np.random.randint(1, self.population_size)
+        for i in range(1, rand):
+            new_population.append(self.snakes[fitness[i][1]])
+        # Shuffling for randomness
+        shuffle(fitness)
+        # Generate new snakes by crossover and mutation
+        for i in range(rand, self.population_size):
+            parent1 = self.select_snake(fitness)
+            parent2 = self.select_snake(fitness)
+            child = parent1.cross_over(parent2)
+            child.mutate(mutation_rate)
+            new_population.append(child)
+        self.snakes = new_population
 
-	def select_snake(self, scores):
-		'''
-		Selects snake for crossover using Roulette Wheel selection strategy
-		'''
-		tsum = 0
-		for score, i in scores:
-			tsum += score
-		rand = np.random.randint(1, int(tsum))
-		curr_sum = 0.
-		for score, i in scores:
-			curr_sum += score
-			if curr_sum >= rand:
-				return self.snakes[i]
-		# Unreachable code
-		assert(False)
+    def select_snake(self, scores):
+        '''
+        Selects snake for crossover using Roulette Wheel selection strategy
+        '''
+        tsum = 0
+        for score, i in scores:
+            tsum += score
+        rand = np.random.randint(1, int(tsum))
+        curr_sum = 0.
+        for score, i in scores:
+            curr_sum += score
+            if curr_sum >= rand:
+                return self.snakes[i]
+        # Unreachable code
+        assert(False)
 
-	def save(self, name):
-		'''
-		Saves the entire population
-		'''
-		filename = 'saved/' + name + '.pickle'
-		with open(filename, 'wb') as f:
-			pickle.dump(self.__dict__, f)
-	
-	def load(self, name):
-		'''
-		Loads the population from saved file
-		'''
-		filename = 'saved/' + name + '.pickle'
-		with open(filename, 'rb') as f:
-			tmp_dict = pickle.load(f)
-		self.__dict__.update(tmp_dict)
+    def save(self, name):
+        '''
+        Saves the entire population
+        '''
+        filename = 'saved/' + name + '.pickle'
+        with open(filename, 'wb') as f:
+            pickle.dump(self.__dict__, f)
+    
+    def load(self, name):
+        '''
+        Loads the population from saved file
+        '''
+        filename = 'saved/' + name + '.pickle'
+        with open(filename, 'rb') as f:
+            tmp_dict = pickle.load(f)
+        self.__dict__.update(tmp_dict)
 
 class Snake:
 
-	def __init__(self, brain=None):
-		'''
-		Creates a new snake
-		Attributes:
-			brain: for specifying brain (not randomly initialized)
-		'''
-		# Initialize the brain
-		self.brain = brain if brain else Brain(nn_config)
+    def __init__(self, brain=None):
+        '''
+        Creates a new snake
+        Attributes:
+            brain: for specifying brain (not randomly initialized)
+        '''
+        # Initialize the brain
+        self.brain = brain if brain else Brain(nn_config)
 
-	def play(self):
-		'''
-		Plays the game and return the score achieved
-		'''
-		g = Game()
-		# Play the game and return the score
-		score, time = g.play(self.brain)
-		self.len = score
-		if score < 10:
-			self.score = math.pow(2, score) * time
-		else:
-			score -= 9
-			self.score = math.pow(2, 10) * time * score * score
-		return self.score
+    def play(self):
+        '''
+        Plays the game and return the score achieved
+        '''
+        g = Game()
+        # Play the game and return the score
+        score, time = g.play(self.brain)
+        self.len = score
+        if score < 10:
+            self.score = math.pow(2, score) * time
+        else:
+            score -= 9
+            self.score = math.pow(2, 10) * time * score * score
+        return self.score
 
-	def train(self):
-		'''
-		Plays snake without pygame for superfast training time.
-		Note: High CPU usage. Don't provide very large number of generations to train in one go.
-		'''
-		g = GameSim()
-		# Play the game and return the score
-		score, time = g.play(self.brain)
-		self.len = score
-		if score < 10:
-			self.score = math.pow(2, score) * time
-		else:
-			score -= 9
-			self.score = math.pow(2, 10) * time * score * score
-		return self.score
-	
-	def clone(self):
-		'''
-		Generates clone of the snake
-		'''
-		b = self.brain.clone()
-		snake = Snake(b)
-		snake.score = self.score
-		snake.len = self.len
-		return snake
-	
-	def mutate(self, prob):
-		'''
-		Mutates the chromosome of the snake
-		Attributes:
-			prob: mutation rate
-		'''
-		self.brain.mutate(prob)
+    def train(self):
+        '''
+        Plays snake without pygame for superfast training time.
+        Note: High CPU usage. Don't provide very large number of generations to train in one go.
+        '''
+        g = GameSim()
+        # Play the game and return the score
+        score, time = g.play(self.brain)
+        self.len = score
+        if score < 10:
+            self.score = math.pow(2, score) * time
+        else:
+            score -= 9
+            self.score = math.pow(2, 10) * time * score * score
+        return self.score
+    
+    def clone(self):
+        '''
+        Generates clone of the snake
+        '''
+        b = self.brain.clone()
+        snake = Snake(b)
+        snake.score = self.score
+        snake.len = self.len
+        return snake
+    
+    def mutate(self, prob):
+        '''
+        Mutates the chromosome of the snake
+        Attributes:
+            prob: mutation rate
+        '''
+        self.brain.mutate(prob)
 
-	def cross_over(self, partner):
-		'''
-		Performs genetic crossover between 'this' snake and partner snake and returns a child snake.
-		'''
-		b = self.brain.cross_over(partner.brain)
-		snake = Snake(b)
-		return snake
+    def cross_over(self, partner):
+        '''
+        Performs genetic crossover between 'this' snake and partner snake and returns a child snake.
+        '''
+        b = self.brain.cross_over(partner.brain)
+        snake = Snake(b)
+        return snake
 
 
 
 if __name__ == '__main__':
-	population = Population(100)
-	population.load('poprp1.1')
-	try:
-		for i in range(500):
-			print('Generation: ', i+1)
-			population.natural_selection()
-	except Exception as e:
-		print(e)
-		population.save('tmp')
-		exit()
-	population.save('poprp1.2')
+    population = Population(100)
+    loadfile = 'poprp1.1'
+    savefile = 'poprp1.2'
+    population.load(loadfile)
+    try:
+        for i in range(500):
+            print('Generation: ', i+1)
+            population.natural_selection()
+    except BaseException as e:	# BaseException can catch even Keyboard interrupt (Ctrl + C)
+        print(e)
+        population.save('tmp')
+        exit()
+    population.save(savefile)
